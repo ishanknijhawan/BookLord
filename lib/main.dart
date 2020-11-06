5bdc3bf09373dac174175b12e037058df84f5510
ishanknijhawan <ishanknijhawan@gmail.com>
2020-11-06 19:57:25 +0530

    test

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Chat App',
+      title: 'BookLord',
       theme: ThemeData(
           primarySwatch: Colors.indigo,
           accentColor: Colors.white,
